1013637e199316bbc5214190ff4520cdd088f8cd
MusienkoYuriy97 <97musienko@gmail.com>
2021-07-26 17:10:05 +0300

    init

---
+++ src/main/java/by/logoped/logopedservice/LogopedServiceApplication.java
@@ -0,0 +1,13 @@
+package by.logoped.logopedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogopedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogopedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/logoped/logopedservice/LogopedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.logoped.logopedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogopedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
